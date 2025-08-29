baa1012944bafa8ac74a1430c26967d993ad3062
Shirasu0703 <219084816+Shirasu0703@users.noreply.github.com>
2025-08-29 10:25:19 +0000

    [Add]いいね機能controller設定まで、次view

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,16 @@
 class Book < ApplicationRecord
   belongs_to :user
+
+  belongs_to :user
+  belongs_to :book
+  has_many :favorites, deoendent: :destroy
+
   validates :title, presence: true
   validates :body, presence: true, length:{maximum:200}
+
+
+  def fovorited_bu?(user)
+    favorites.exists?(user_id: user.id)
+  
+  
 end
