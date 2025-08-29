c0d6febf333a8a34b81a97b14c5edcf9bbc14cad
Ubuntu <ubuntu@ip-172-31-42-88.ap-northeast-1.compute.internal>
2025-08-29 10:25:19 +0000

    [Add]いいね機能controller設定まで、次view

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,16 @@
 class User < ApplicationRecord
   has_many :books, dependent: :destroy
+
+  belongs_to :user
+  belongs_to :book
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+         
   has_one_attached :profile_image
+  has_many :favorites, dependent: :destroy
 
   validates :name, presence: true, uniqueness: true, length: { minimum: 2, maximum: 20 }
   validates :introduction, length: { maximum: 50 }
