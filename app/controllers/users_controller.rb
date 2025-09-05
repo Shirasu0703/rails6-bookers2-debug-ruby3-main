4d20fc83e900a6ec584e1b9700bd6ddb9fee55cf
Ubuntu <ubuntu@ip-172-31-42-88.ap-northeast-1.compute.internal>
2025-09-05 08:01:22 +0000

    commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,6 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
   before_action :is_matching_login_user, only: [:edit, :update, :destroy]
 
-  
   def show
     @user = User.find(params[:id])
     @books = @user.books
