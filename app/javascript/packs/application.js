b381ff4aa0b834b3b06c6dff07cd922e5a0b9d33
Shirasu0703 <219084816+Shirasu0703@users.noreply.github.com>
2025-09-07 06:24:42 +0000

    [Finish]課題7d

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -18,9 +18,9 @@ Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
 
-import Raty from "raty.js"
-window.raty = function(elem,opt) {
-  let raty = new Raty(elem,opt)
+import Raty from "./raty.js"
+window.raty = function(elem,opt){
+  var raty = new Raty(elem,opt)
   raty.init();
   return raty;
 }
