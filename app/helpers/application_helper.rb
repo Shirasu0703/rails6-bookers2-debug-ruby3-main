2796bcbb2ddf444baa988bf488281f6936e72e05
Shirasu0703 <219084816+Shirasu0703@users.noreply.github.com>
2025-09-08 11:30:11 +0000

    [Update]課題7~9d復習

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  def exclude_partial?
+    # トップページは常に非表示
+    return true if current_page?(root_path)
+
+    # ログインしている場合は表示(root以外で)
+    # →root_path　以外でログインしていれば false になるので表示される
+
+    # ユーザー編集ページの非表示
+    return true if controller_name == 'users' && action_name == "edit"
+
+    # 投稿編集ページの非表示
+    return true if controller_name == 'books' && action_name == 'edit'
+
+    false
+  end
 end
